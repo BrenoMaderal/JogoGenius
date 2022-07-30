67e45eeb0ec6314a026dfb029db38fd96d5c895a
BrenoMaderal <breninhomaderalortiz@gmail.com>
2022-07-30 11:58:27 -0300

    final

--- js/scripts.js
+++ js/scripts.js
@@ -44,6 +44,7 @@ _gui.switch.addEventListener("click", () => {
 	_data.playerSequence = [];
 
 	disablePads();
+	changePadCursor("auto");
 
 	_gui.led.classList.remove("gui__led--active");
 
@@ -74,6 +75,7 @@ const padListener = (e) => {
 	_data.sounds[soundId].play();
 	_data.playerSequence.push(soundId);
 
+setTimeout(() => {
 	e.target.classList.remove("game__pad--active");
 
 	const currentMove = _data.playerSequence.length - 1;
@@ -81,12 +83,14 @@ const padListener = (e) => {
 	if (_data.playerSequence[currentMove] !== _data.gameSequence[currentMove]) {
 		_data.playerCanPlay = false;
 		disablePads();
-		playSequence();
+		resetOrPlayAgain();
 	}
 	else if (currentMove === _data.gameSequence.length - 1) {
 		newColor();
 		playSequence();
 	}
+waitForPlayerClick();
+}, 250);
 }
 
 _gui.pads.forEach(pad => {
@@ -107,6 +111,10 @@ const setScore = () => {
 }
 
 const newColor = () => {
+	if(_data.score === 20){
+		blink("**", startGame);
+		return;
+	}
 	_data.gameSequence.push(Math.floor(Math.random() * 4));
 	_data.score++;
 
@@ -120,6 +128,8 @@ const playSequence = () => {
 	_data.playerSequence = [];
 	_data.playerCanPlay = false
 
+	changePadCursor("auto");
+
 	const interval = setInterval(() => {
 		if (!_data.gameOn) {
 			clearInterval(interval);
@@ -132,6 +142,7 @@ const playSequence = () => {
 				clearInterval(interval);
 				disablePads();
 				waitForPlayerClick();
+				changePadCursor("pointer");
 				_data.playerCanPlay = true;
 				return;
 			}
@@ -189,16 +200,32 @@ const waitForPlayerClick = () => {
 			return;
 
 		disablePads();
-		playSequence();
+		resetOrPlayAgain();
 	}, 5000);
 }
 
 const resetOrPlayAgain = () => {
+_data.playerCanPlay = false;
 
+if(_data.strict){
+	blink("!!", () => {
+		_data.score = 0;
+		_data.gameSequence = [];
+		startGame();
+	});
+}
+else {
+	blink("!!", () => {
+		setScore();
+		playSequence();
+	 });
+   }
 }
 
 const changePadCursor = (cursorType) => {
-
+    _gui.pads.forEach(pad => {
+		pad.style.cursor = cursorType;
+	});
 }
 
 const disablePads = () => {
